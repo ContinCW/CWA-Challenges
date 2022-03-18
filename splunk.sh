c004de99795c4e91585c5ee58592291d8799bf79
Contin <rcont@cyberwarrior.com>
2022-03-18 18:02:40 +0000

    updating

--- splunk.sh
+++ splunk.sh
@@ -2,11 +2,17 @@
 echo "Deploying Splunk from a Docker Instance on Ubuntu Server 20 by Rafael Contin CW"
 admin_password=CyberWarriorSplunk
 
-apt-get update && sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+sudo apt-get update
+
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \ "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu/ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose version
@@ -49,6 +55,15 @@ echo "CHECKING STATUS..."
 sleep 5
 echo "CHECKING STATUS..."
 sleep 5
+echo "CHECKING STATUS..."
+sleep 5
+echo "CHECKING STATUS..."
+sleep 5
+echo "CHECKING STATUS..."
+sleep 5
+echo "CHECKING STATUS..."
+sleep 5
+
 
 docker ps
 echo " "
